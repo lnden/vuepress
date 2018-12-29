6c18f0958310a5731adf2c8f9e543d822bfd34f2
dinglindong <258231674@qq.com>
2018-12-29 18:24:27 +0800

    add page nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,9 +8,76 @@ module.exports = {
     themeConfig: {
         navbar:true,
         nav: [
-            { text: 'Home', link: '/accumulate/' },
+            { text: 'FE-基础',  link: '/basics/' },
+            { text: 'FE-框架',  link: '/frame/' },
+            { text: 'FE-面试',  link: '/interview/' },
+            { text: '年终总结', link: '/summary/' },
+            { text: 'GitHub',  link: 'https://google.com' },
         ],
+        sidebar:{
+            '/basics/': getBasics('FE-基础'),
+            '/frame/': getFrame('FE-框架'),
+            '/interview/': getInterview('FE-面试'),
+            '/summary/': getSummary('年终总结')
+        },
         sidebarDepth:2,
-        lastUpdated: 'Last Updated'
+        lastUpdated: 'Last Updated',
+        search:true
     }
 };
+
+
+function getBasics(title) {
+    return [
+        {
+            title,
+            collapsable: false,
+            children: [
+                '',
+                'demo'
+            ]
+        }
+    ]
+}
+
+function getFrame(title) {
+    return  [
+        {
+            title,
+            collapsable: false,
+            children: [
+                '',
+                'vue',
+                'vuex',
+                'react',
+                'redux',
+                'angular',
+                'weChat'
+            ]
+        }
+    ]
+}
+
+function getInterview(title) {
+    return  [
+        {
+            title,
+            collapsable: false,
+            children: [
+                ''
+            ]
+        }
+    ]
+}
+
+function getSummary(title) {
+    return [
+        {
+            title,
+            collapsable: false,
+            children: [
+                ''
+            ]
+        }
+    ]
+}
